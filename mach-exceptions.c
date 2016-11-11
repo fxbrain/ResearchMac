332901ae34c7b4b16bc0b800eaa879af3b3cecf2
Stefan Schwarz <stefan.schwarz@redmedical.de>
2016-11-11 19:20:27 +0100

    Initial commit

---
+++ mach-exceptions.c
@@ -0,0 +1,217 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <IOKit/IOReturn.h>
+#include <mach/mach.h>
+#include <pthread.h>
+
+#include <mach/exc.h>
+//#include <mach/i386/thread_status.h>
+//#include <mach/mach_error.h>
+//#include <mach/mach_init.h>
+//#include <mach/mach_port.h>
+//#include <mach/task.h>
+//#include <mach/thread_act.h>
+//#include <mach/thread_status.h>
+
+#define THREAD_STATE_FLAVOR 0
+
+#ifdef __MigPackStructs
+#pragma pack(4)
+#endif
+typedef struct {
+    mach_msg_header_t Head;
+    /* start of the kernel processed data */
+    mach_msg_body_t msgh_body;
+    mach_msg_port_descriptor_t thread;
+    mach_msg_port_descriptor_t task;
+    /* end of the kernel processed data */
+    NDR_record_t NDR;
+    exception_type_t exception;
+    mach_msg_type_number_t codeCnt;
+    int64_t code[2];
+    int flavor;
+    mach_msg_type_number_t old_stateCnt;
+    natural_t old_state[224];
+} __Request__mach_exception_raise_state_identity_t;
+#ifdef __MigPackStructs
+#pragma pack()
+#endif
+
+/* Local short names, for convenience. */
+typedef __Request__mach_exception_raise_state_identity_t protRequestStruct;
+typedef __Reply__exception_raise_state_identity_t protReplyStruct;
+
+static mach_port_name_t protExcPort = MACH_PORT_NULL;
+
+static void protBuildReply(protReplyStruct* reply, protRequestStruct* request,
+        kern_return_t ret_code)
+{
+    mach_msg_size_t state_size;
+    reply->Head.msgh_bits =
+            MACH_MSGH_BITS(MACH_MSGH_BITS_REMOTE(request->Head.msgh_bits), 0);
+    reply->Head.msgh_remote_port = request->Head.msgh_remote_port;
+    reply->Head.msgh_local_port = MACH_PORT_NULL;
+    reply->Head.msgh_reserved = 0;
+    reply->Head.msgh_id = request->Head.msgh_id+100;
+    reply->NDR = request->NDR;
+    reply->RetCode = ret_code;
+    reply->flavor = request->flavor;
+    reply->new_stateCnt = request->old_stateCnt;
+    state_size = reply->new_stateCnt*sizeof(natural_t);
+    memcpy(reply->new_state, request->old_state, state_size);
+    /* If you use sizeof(reply) for reply->Head.msgh_size then the state
+       gets ignored. */
+    reply->Head.msgh_size =
+            offsetof(protReplyStruct, new_state)+state_size;
+}
+
+static void protMustSend(mach_msg_header_t* head)
+{
+    kern_return_t kr;
+    kr = mach_msg(head, MACH_SEND_MSG, head->msgh_size,
+            /* recv_size */ 0, MACH_PORT_NULL, MACH_MSG_TIMEOUT_NONE,
+            MACH_PORT_NULL);
+    if (kr!=KERN_SUCCESS) {
+        mach_error("ERROR: MPS mach_msg send", kr); /* .trans.must */
+    }
+}
+
+static void protCatchOne(void)
+{
+    protRequestStruct request;
+    mach_msg_return_t mr;
+    protReplyStruct reply;
+
+    mr = mach_msg(&request.Head,
+            /* option */ MACH_RCV_MSG,
+            /* send_size */ 0,
+            /* receive_limit */ sizeof(request),
+            /* receive_name */ protExcPort,
+            /* timeout */ MACH_MSG_TIMEOUT_NONE,
+            /* notify */ MACH_PORT_NULL);
+    if (mr!=MACH_MSG_SUCCESS) {
+        mach_error("ERROR: MPS mach_msg recv\n", mr); /* .trans.must */
+    }
+    protBuildReply(&reply, &request, KERN_SUCCESS);
+    protMustSend(&reply.Head);
+    return;
+}
+
+static void* protCatchThread(void* p)
+{
+    for (; ;) {
+        protCatchOne();
+    }
+}
+
+extern void ProtThreadRegister(bool setup)
+{
+    kern_return_t kr;
+    mach_msg_type_number_t old_exception_count;
+    exception_mask_t old_exception_masks;
+    exception_behavior_t behavior;
+    mach_port_t old_exception_ports;
+    exception_behavior_t old_behaviors;
+    thread_state_flavor_t old_flavors;
+    mach_port_t self;
+    static mach_port_t setupThread = MACH_PORT_NULL;
+
+    self = mach_thread_self();
+
+    /* Avoid setting up the exception handler for the thread that calls
+       ProtSetup twice, in the case where the mutator registers that thread
+       explicitly.  We need a special case because we don't require thread
+       registration of the sole thread of a single-threaded mutator. */
+    if (setup) {
+        setupThread = self;
+    }
+    else {
+        if (self==setupThread) {
+            return;
+        }
+    }
+
+    /* Ask to receive EXC_BAD_ACCESS exceptions on our port, complete
+       with thread state and identity information in the message.
+       The MACH_EXCEPTION_CODES flag causes the code fields to be
+       passed 64-bits wide, matching protRequestStruct [Fuller_2013]. */
+    behavior = (exception_behavior_t) (EXCEPTION_STATE_IDENTITY |
+            MACH_EXCEPTION_CODES);
+    kr = thread_swap_exception_ports(
+            self, EXC_MASK_BAD_ACCESS, protExcPort, behavior, THREAD_STATE_NONE,
+            &old_exception_masks, &old_exception_count, &old_exception_ports,
+            &old_behaviors, &old_flavors);
+    if (kr!=KERN_SUCCESS) {
+        mach_error("ERROR: MPS thread_swap_exception_ports",
+                kr); /* .trans.must */
+    }
+}
+
+static void protSetupInner(void)
+{
+    kern_return_t kr;
+    int pr;
+    pthread_t excThread;
+    mach_port_t self;
+
+    /* Create a port to send and receive exceptions. */
+    self = mach_task_self();
+    kr = mach_port_allocate(self, MACH_PORT_RIGHT_RECEIVE, &protExcPort);
+    if (kr!=KERN_SUCCESS) {
+        mach_error("ERROR: MPS mach_port_allocate",
+                kr); /* .trans.must */
+    }
+
+    /* Allow me to send exceptions on this port. */
+    /* TODO: Find out why this is necessary. */
+    self = mach_task_self();
+    kr = mach_port_insert_right(self, protExcPort, protExcPort,
+            MACH_MSG_TYPE_MAKE_SEND);
+
+    if (kr!=KERN_SUCCESS) {
+        mach_error("ERROR: MPS mach_port_insert_right",
+                kr); /* .trans.must */
+    }
+
+    ProtThreadRegister(true);
+
+    /* Launch the exception handling thread.  We use pthread_create because
+       it's much simpler than setting up a thread from scratch using Mach,
+       and that's basically what it does.  See [Libc]
+       <http://www.opensource.apple.com/source/Libc/Libc-825.26/pthreads/pthread.c>
+       */
+    pr = pthread_create(&excThread, NULL, protCatchThread, NULL);
+    if (pr!=0) {
+        fprintf(stderr, "ERROR: MPS pthread_create: %d\n",
+                pr); /* .trans.must */
+    }
+}
+
+void ProtSetup(void)
+{
+    int pr;
+    static pthread_once_t prot_setup_once = PTHREAD_ONCE_INIT;
+
+    pr = pthread_once(&prot_setup_once, protSetupInner);
+    if (pr!=0) {
+        fprintf(stderr, "ERROR: MPS pthread_once: %d\n",
+                pr); /* .trans.must */
+    }
+}
+
+int main()
+{
+    uint32_t eax, ebx, ecx, edx;
+    char str[9];
+    char PSN[30];
+
+    ProtSetup();
+
+    return 0;
+}
+
+
---
+++ usb-iteration.c
@@ -0,0 +1,431 @@
+//
+// Created by Stefan Schwarz on 11/11/2016.
+//
+
+#include <stdint.h>
+#include <IOKit/usb/IOUSBLib.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/IOReturn.h>
+#include <IOKit/IOCFPlugIn.h>
+#include <IOKit/usb/USBSpec.h>
+#include <IOKit/serial/IOSerialKeys.h>
+#include <sys/param.h>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+
+enum USBDeviceErrorCode
+{
+    kSuccess                        = 0,        // description...
+    kFailure                        = 1 << 0,   // description...
+    kDeviceNotOnline                = 1 << 1,   // description...
+    kActiveConfigFailed             = 1 << 2,   // description...
+    kDeviceInUse                    = 1 << 3,   // description...
+};
+
+
+IOReturn getDeviceManufacturer(IOUSBDeviceInterface650** usbDevice,
+        CFStringRef* manufString)
+{
+    UInt8 stringIndex;
+    IOUSBDevRequest devRequest;
+    UInt8 buffer[256];
+    IOReturn error;
+
+    // Get the index in the string table for the manufacturer
+    error = (*usbDevice)->USBGetManufacturerStringIndex(usbDevice, &stringIndex);
+    if (error!=kIOReturnSuccess) {
+        return error;
+    }
+
+    // Perform a device request to read the string descriptor
+    devRequest.bmRequestType = USBmakebmRequestType(kUSBIn, kUSBStandard, kUSBDevice);
+    devRequest.bRequest = kUSBRqGetDescriptor;
+    devRequest.wValue = (kUSBStringDesc << 8) | stringIndex;
+    devRequest.wIndex = 0x409;        // Language setting - specify US English
+    devRequest.wLength = sizeof(buffer);
+    devRequest.pData = &buffer[0];
+    bzero(&buffer[0], sizeof(buffer));
+    //
+    error = (*usbDevice)->DeviceRequest(usbDevice, &devRequest);
+    if (error!=kIOReturnSuccess) {
+        return error;
+    }
+
+    // Create a CFString representation of the returned data
+    int strLength;
+    strLength = buffer[0]-2;        // First byte is length (in bytes)
+    (*manufString) = CFStringCreateWithBytes(kCFAllocatorDefault, &buffer[2], strLength,
+            kCFStringEncodingUTF16LE, false);
+    return error;
+}
+
+/**
+ Fetches the product name of the usbDevice
+
+ @param usbDevice the actual usb device
+ @param prodString the returned name of the device
+ @return the result code of the kernel operation
+ */
+IOReturn getProductName(IOUSBDeviceInterface650** usbDevice, CFStringRef* prodString)
+{
+    UInt8 stringIndex;
+    IOUSBDevRequest devRequest;
+    UInt8 buffer[256];
+    IOReturn error;
+
+    // Get the index in the string table for the manufacturer
+    error = (*usbDevice)->USBGetProductStringIndex(usbDevice, &stringIndex);
+    if (error!=kIOReturnSuccess) {
+        return error;
+    }
+
+    // Perform a device request to read the string descriptor
+    devRequest.bmRequestType = USBmakebmRequestType(kUSBIn, kUSBStandard, kUSBDevice);
+    devRequest.bRequest = kUSBRqGetDescriptor;
+    devRequest.wValue = (kUSBStringDesc << 8) | stringIndex;
+    devRequest.wIndex = 0x409;        // Language setting - specify US English
+    devRequest.wLength = sizeof(buffer);
+    devRequest.pData = &buffer[0];
+    bzero(&buffer[0], sizeof(buffer));
+    //
+    error = (*usbDevice)->DeviceRequest(usbDevice, &devRequest);
+    if (error!=kIOReturnSuccess) {
+        return error;
+    }
+
+    // Create a CFString representation of the returned data
+    int strLength;
+    strLength = buffer[0]-2;        // First byte is length (in bytes)
+    (*prodString) = CFStringCreateWithBytes(kCFAllocatorDefault, &buffer[2],
+            strLength, kCFStringEncodingUTF16LE,
+            false);
+    return error;
+}
+
+/**
+ Returns the serial number as a CFString.
+ It is the caller's responsibility to release the returned CFString when done with it.
+
+ @param serialNumber OUT: the serial number
+ */
+void HostSerialNumber(CFStringRef* serialNumber)
+{
+    if (serialNumber!=NULL) {
+        *serialNumber = NULL;
+
+        io_service_t platformExpert =
+                IOServiceGetMatchingService(kIOMasterPortDefault,
+                        IOServiceMatching("IOPlatformExpertDevice"));
+
+        if (platformExpert) {
+            CFTypeRef serialNumberAsCFString =
+                    IORegistryEntryCreateCFProperty(platformExpert,
+                            CFSTR(kIOPlatformSerialNumberKey),
+                            kCFAllocatorDefault, 0);
+            if (serialNumberAsCFString) {
+                *serialNumber = serialNumberAsCFString;
+            }
+
+            IOObjectRelease(platformExpert);
+        }
+    }
+}
+
+const char* CFStringCopyUTF8String(CFStringRef aString)
+{
+    if (aString==NULL) {
+        return NULL;
+    }
+
+    CFIndex length = CFStringGetLength(aString);
+    CFIndex maxSize = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8)+1;
+    char* buffer = (char*) malloc(maxSize);
+
+    if (CFStringGetCString(aString, buffer, maxSize, kCFStringEncodingUTF8)) {
+        return buffer;
+    }
+
+    return NULL;
+}
+
+/**
+ Investigates the actual device file for the given
+ Vendor/Product identifiers
+
+ @param idVendor the vendor id
+ @param idProduct the product id
+ @param deviceFilePath the actual file path of the device file in /dev
+ @return the result code of the operation
+ */
+enum USBDeviceErrorCode getDeviceFilePath(SInt32 idVendor, SInt32 idProduct,
+        char* deviceFilePath)
+{
+    CFMutableDictionaryRef matchingDictionary = NULL;
+    io_iterator_t iterator = 0;
+    io_service_t usbRef;
+    SInt32 score;
+    IOCFPlugInInterface** plugin;
+    IOUSBDeviceInterface650** usbDevice = NULL;
+    IOUSBFindInterfaceRequest interfaceRequest;
+
+    matchingDictionary = IOServiceMatching(kIOUSBDeviceClassName);
+    CFDictionaryAddValue(matchingDictionary, CFSTR(kUSBVendorID),
+            CFNumberCreate(kCFAllocatorDefault,
+                    kCFNumberSInt32Type, &idVendor));
+    CFDictionaryAddValue(matchingDictionary, CFSTR(kUSBProductID),
+            CFNumberCreate(kCFAllocatorDefault,
+                    kCFNumberSInt32Type, &idProduct));
+    IOServiceGetMatchingServices(kIOMasterPortDefault,
+            matchingDictionary, &iterator);
+    usbRef = IOIteratorNext(iterator);
+
+    if (usbRef==0) {
+        fprintf(stderr, "Device is not online.\n");
+        return kDeviceNotOnline;
+    }
+
+    IOObjectRelease(iterator);
+    IOCreatePlugInInterfaceForService(usbRef, kIOUSBDeviceUserClientTypeID,
+            kIOCFPlugInInterfaceID, &plugin, &score);
+    IOObjectRelease(usbRef);
+    (*plugin)->QueryInterface(plugin, CFUUIDGetUUIDBytes(kIOUSBDeviceInterfaceID300),
+            (LPVOID) &usbDevice);
+
+    (*plugin)->Release(plugin);
+
+    interfaceRequest.bInterfaceClass = kIOUSBFindInterfaceDontCare;
+    interfaceRequest.bInterfaceSubClass = kIOUSBFindInterfaceDontCare;
+    interfaceRequest.bInterfaceProtocol = kIOUSBFindInterfaceDontCare;
+    interfaceRequest.bAlternateSetting = kIOUSBFindInterfaceDontCare;
+
+    (*usbDevice)->CreateInterfaceIterator(usbDevice, &interfaceRequest, &iterator);
+
+    IOIteratorNext(iterator); // skip interface #0
+    usbRef = IOIteratorNext(iterator);
+    IOObjectRelease(iterator);
+
+    CFStringRef deviceBSDName_cf =
+            (CFStringRef) IORegistryEntrySearchCFProperty(usbRef,
+                    kIOServicePlane,
+                    CFSTR (kIOCalloutDeviceKey),
+                    kCFAllocatorDefault,
+                    kIORegistryIterateRecursively);
+    if (deviceBSDName_cf) {
+        Boolean result;
+
+        // Convert the path from a CFString to a NULL-terminated C string
+        // for use with the POSIX open() call.
+
+        result = CFStringGetCString(deviceBSDName_cf,
+                deviceFilePath,
+                1024,
+                kCFStringEncodingASCII);
+        CFRelease(deviceBSDName_cf);
+
+        if (result) {
+            return kSuccess;
+        }
+    }
+    return kFailure;
+}
+
+/**
+ Checks if the specified device through Vendor/Product pair
+ is online and usable.
+
+ @param idVendor the vendor id
+ @param idProduct the product id
+ @return the result code of the operation
+ */
+enum USBDeviceErrorCode checkDeviceState(SInt32 idVendor, SInt32 idProduct)
+{
+    CFMutableDictionaryRef matchingDictionary = NULL;
+    io_iterator_t iterator = 0;
+    io_service_t usbRef;
+    SInt32 score;
+    IOCFPlugInInterface** plugin;
+    //    IOUSBDeviceInterface300** usbDevice = NULL;
+    IOUSBDeviceInterface650** usbDevice = NULL;
+    IOReturn ret;
+    IOUSBConfigurationDescriptorPtr config;
+    IOUSBFindInterfaceRequest interfaceRequest;
+    IOUSBInterfaceInterface300** usbInterface;
+    CFStringRef manufString, prodString;
+    CFStringRef serialNumberHost;
+    //    cuPrivateData   *privateDataRef = NULL;
+    UInt32 locationID, bandwith;
+    UInt16 vendor, product;
+
+    HostSerialNumber(&serialNumberHost);
+    printf("Host-Serial-Number is %s\n", CFStringCopyUTF8String(serialNumberHost));
+    CFRelease(serialNumberHost);
+
+    /* try to find USB device using the vendor and product id */
+    matchingDictionary = IOServiceMatching(kIOUSBDeviceClassName);
+    CFDictionaryAddValue(matchingDictionary, CFSTR(kUSBVendorID),
+            CFNumberCreate(kCFAllocatorDefault,
+                    kCFNumberSInt32Type, &idVendor));
+    CFDictionaryAddValue(matchingDictionary, CFSTR(kUSBProductID),
+            CFNumberCreate(kCFAllocatorDefault,
+                    kCFNumberSInt32Type, &idProduct));
+    IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDictionary, &iterator);
+    usbRef = IOIteratorNext(iterator);
+
+    if (usbRef==0) {
+        fprintf(stderr, "Device is not online.\n");
+        return kDeviceNotOnline;
+    }
+    IOObjectRelease(iterator);
+    IOCreatePlugInInterfaceForService(usbRef, kIOUSBDeviceUserClientTypeID,
+
+            kIOCFPlugInInterfaceID, &plugin, &score);
+    IOObjectRelease(usbRef);
+    (*plugin)->QueryInterface(plugin, CFUUIDGetUUIDBytes(kIOUSBDeviceInterfaceID300),
+            (LPVOID) &usbDevice);
+
+    (*plugin)->Release(plugin);
+
+    // The locationID uniquely identifies the device
+    // and will remain the same, even across reboots, so long as the bus
+    // topology doesn't change.
+    (*usbDevice)->GetLocationID(usbDevice, &locationID);
+    (*usbDevice)->GetDeviceVendor(usbDevice, &vendor);
+    (*usbDevice)->GetDeviceProduct(usbDevice, &product);
+    (*usbDevice)->GetBandwidthAvailableForDevice(usbDevice, &bandwith);
+
+    fprintf(stdout, "LocationID is : 0x%x\n", locationID);
+    fprintf(stdout, "VendorID is : 0x%x\n", vendor);
+    fprintf(stdout, "ProductID is : 0x%x\n", product);
+
+    getDeviceManufacturer(usbDevice, &manufString);
+    getProductName(usbDevice, &prodString);
+
+    /*
+     * now that we have found the device, we open the device and set the
+     * first configuration as active
+     */
+
+    ret = (*usbDevice)->USBDeviceOpen(usbDevice);
+    if (ret==kIOReturnSuccess) {
+        // set first configuration as active
+        ret = (*usbDevice)->GetConfigurationDescriptorPtr(usbDevice, 0, &config);
+        if (ret!=kIOReturnSuccess) {
+            printf("Could not set active configuration (error: %x)\n", ret);
+            return kActiveConfigFailed;
+        }
+        (*usbDevice)->SetConfiguration(usbDevice, config->bConfigurationValue);
+    }
+    interfaceRequest.bInterfaceClass = kIOUSBFindInterfaceDontCare;
+    interfaceRequest.bInterfaceSubClass = kIOUSBFindInterfaceDontCare;
+    interfaceRequest.bInterfaceProtocol = kIOUSBFindInterfaceDontCare;
+    interfaceRequest.bAlternateSetting = kIOUSBFindInterfaceDontCare;
+
+    (*usbDevice)->CreateInterfaceIterator(usbDevice, &interfaceRequest, &iterator);
+
+    IOIteratorNext(iterator); // skip interface #0
+    usbRef = IOIteratorNext(iterator);
+    IOObjectRelease(iterator);
+
+    IOCreatePlugInInterfaceForService(usbRef, kIOUSBInterfaceUserClientTypeID,
+            kIOCFPlugInInterfaceID, &plugin, &score);
+    IOObjectRelease(usbRef);
+
+    (*plugin)->QueryInterface(
+            plugin,
+            CFUUIDGetUUIDBytes(kIOUSBInterfaceInterfaceID300),
+            (LPVOID) &usbInterface
+    );
+    (*plugin)->Release(plugin);
+
+    if (usbInterface==NULL) {
+        printf("Device " BOLDMAGENTA "%s : %s" RESET  \
+               "cannot be opened. [ " RED "ERROR" RESET " ]\n",
+                CFStringCopyUTF8String(manufString),
+                CFStringCopyUTF8String(prodString));
+        CFRelease(manufString);
+        CFRelease(prodString);
+        return kDeviceInUse;
+    }
+    ret = (*usbInterface)->USBInterfaceOpen(usbInterface);
+    if (ret!=kIOReturnSuccess) {
+        if (ret==kIOReturnExclusiveAccess) {
+            printf("Device " BOLDMAGENTA "%s : %s" RESET \
+                   ", is already in use. [ " RED "ERROR" RESET " ]\n",
+                    CFStringCopyUTF8String(manufString),
+                    CFStringCopyUTF8String(prodString));
+            CFRelease(manufString);
+            CFRelease(prodString);
+            return kDeviceInUse;
+        }
+    }
+
+    printf("Device " BOLDMAGENTA "%s : %s" RESET \
+           ", is ready to use. [ " GREEN "OK" RESET " ]\n",
+            CFStringCopyUTF8String(manufString),
+            CFStringCopyUTF8String(prodString));
+    CFRelease(manufString);
+    CFRelease(prodString);
+
+    (*usbInterface)->USBInterfaceClose(usbInterface);
+    (*usbDevice)->USBDeviceClose(usbDevice);
+
+    return kSuccess;
+}
+
+/**
+ * Lists the available usb devices registered inside the system.
+ * @return the result code of the operation
+ */
+enum USBDeviceErrorCode listDevices()
+{
+    CFMutableDictionaryRef matchingDictionary = NULL;
+    IOCFPlugInInterface** plugin;
+    IOUSBDeviceInterface650** usbDevice = NULL;
+    SInt32 score;
+    io_iterator_t iter = 0;
+    io_service_t device;
+    kern_return_t kr;
+    UInt16 vendor, product;
+
+    matchingDictionary = IOServiceMatching(kIOUSBDeviceClassName);
+    if (matchingDictionary==NULL) {
+        return kFailure;
+    }
+
+    kr = IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDictionary, &iter);
+    if (kr!=KERN_SUCCESS) {
+        return kFailure;
+    }
+
+    while ((device = IOIteratorNext(iter))) {
+        IOCreatePlugInInterfaceForService(device, kIOUSBDeviceUserClientTypeID,
+                kIOCFPlugInInterfaceID, &plugin, &score);
+        (*plugin)->QueryInterface(plugin,
+                CFUUIDGetUUIDBytes(kIOUSBDeviceInterfaceID300),
+                (LPVOID) &usbDevice);
+        (*plugin)->Release(plugin);
+
+        (*usbDevice)->GetDeviceVendor(usbDevice, &vendor);
+        (*usbDevice)->GetDeviceProduct(usbDevice, &product);
+
+        fprintf(stdout, "0x%04x : 0x%04x\n", vendor, product);
+        fprintf(stdout, "\n\n");
+        IOObjectRelease(device);
+    }
+    return kSuccess;
+}
+
+
+int main(int argc, char **argv)
+{
+    char path[MAXPATHLEN];
+    listDevices();
+    checkDeviceState(0x05ac, 0x8290);
+    getDeviceFilePath(0x05ac, 0x8290, path);
+    fprintf(stdout, "Device Path : %s\n", path);
+    return EXIT_SUCCESS;
+}
